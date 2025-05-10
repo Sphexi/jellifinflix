1dd4c74507584d3a2fbccf083f52accbd6b56aa6
Sphexi <git@sphexi.com>
2025-05-10 11:13:23 -0700

    More fixes
    Removed the double quotes from the docker-compose file for the cron string, they were causing an issue in the root user crontab file and breaking the schedule.
    
    Also removed an additional space from the same echo line.

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 figlet -w 120 -l Jellyfin_Fix
 echo "Server started at $(date)"
 echo "Running cron job $CRON_STRING"
-echo "$CRON_STRING  sh /app/entry.sh" > /app/crontab
+echo "$CRON_STRING sh /app/entry.sh" > /app/crontab
 crontab /app/crontab
 cat /app/crontab
 # start cron
